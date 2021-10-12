4abb52bf9f1542fa451a613b21b1f8212421e3d0
sciug <gaetano.sciurti@libero.it>
2021-10-12 17:49:15 +0200

    fixed rotation issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,13 @@ var text = document.getElementById("testo")
 
 bluePill.addEventListener("click", function(){
     redPill.classList.add("hidden")
-   text.innerHTML = `The story ends, You wake up in your bed and believe whatever you want to believe` 
+   text.innerHTML = `The story ends, You wake up in your bed and believe whatever you want to believe...` 
 
 })
 
 
 redPill.addEventListener("click", function(){
     bluePill.classList.add("hidden")
-    text.innerHTML = "You stay in Wonderland, and I show you how deep the rabbit hole goes." 
+    text.innerHTML = "You stay in Wonderland, and I show you how deep the rabbit hole goes..." 
 })
 
